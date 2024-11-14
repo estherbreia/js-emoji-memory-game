06370f0f4814bbca6d405e05914b876cc93cad7c
estherbreia <estherbreia@gmail.com>
2024-11-14 10:29:18 -0300

    feat: add jogo da memória

---
+++ src/scripts/engine.js
@@ -0,0 +1,61 @@
+
+const emojis = [
+    "🖖",
+    "🖖",
+    "😒",
+    "😒",
+    "😅",
+    "😅",
+    "😎",
+    "😎",
+    "😜",
+    "😜",
+    "🤡",
+    "🤡",
+    "👽",
+    "👽",
+    "🐸",
+    "🐸",
+];
+
+let openCards = [];
+
+// Embaralha os emojis
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+// Cria as cards dos emojis
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box)
+}
+
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if (openCards.length == 2) {
+        setTimeout(checkMath, 500)
+    }
+}
+
+// Condição para verificar se as cards são iguais e adicionar e remover classes
+function checkMath() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert('Você venceu!')
+    }
+}
